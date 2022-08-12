fdf7eff8d848b58d2cc872de8b23ff87be37b59b
Wouter Gerrits <wouter@cuneocashflow.com>
2022-08-12 14:13:11 +0200

    Add navigation routing

--- lib/components/navigation_view_component.dart
+++ lib/components/navigation_view_component.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_exam_wouter/blocs/navbar_bloc/navbar_bloc.dart';
+import 'package:flutter_exam_wouter/views/privacy/privacy_view.dart';
+import 'package:flutter_exam_wouter/views/projects/projects_view.dart';
 
 class NavigationViewComponent extends StatelessWidget {
   const NavigationViewComponent({Key? key}) : super(key: key);
 
   getView(currentIndex) {
     if (currentIndex == 0) {
-      return const Text('Project');
+      return const ProjectsView();
     } else if (currentIndex == 1) {
-      return const Text('Privacy');
+      return const PrivacyView();
     }
 
     return Container();
